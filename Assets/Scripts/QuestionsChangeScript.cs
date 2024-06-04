7202b664bc6143dbe62a2c6b52db71f59d286de3
Miirflin <mrmiirflinn@gmail.com>
2024-06-04 22:04:49 +0200

    Starting to write change question script

--- Assets/Scripts/QuestionsChangeScript.cs
+++ Assets/Scripts/QuestionsChangeScript.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionsChangeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Buttons;
+    public Text Number;
+    public Text QuestionDes;
+
+    
+    private int Question = 0;
+    private string[] Question_list = { };
+    private bool[] submited = new bool[15];
+
+    private void Start()
     {
-        
+        for(int i = 0; i < 15; i++)
+        {
+            submited[i] = false;
+        }
+        Number.text = (Question+1).ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
