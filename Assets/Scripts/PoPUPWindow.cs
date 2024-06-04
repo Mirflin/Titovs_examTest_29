31799eb9f53265163185c7b61bc7b3150a559fa3
Miirflin <mrmiirflinn@gmail.com>
2024-06-04 21:50:03 +0200

    Main question menu design

--- Assets/Scripts/PoPUPWindow.cs
+++ Assets/Scripts/PoPUPWindow.cs
@@ -10,15 +10,10 @@ public class PoPUPWindow : MonoBehaviour
     
     private void OnMouseEnter()
     {
-        Debug.Log("Trigger enter!");
-        Vector3 NewPosition = new Vector3(678,0,-9720);
-        Transform Trans = Window.GetComponent<Transform>();
-        Trans.position = (new Vector3(Trans.position.x + -600, 0, 0));
+        Window.SetActive(true);
     }
     private void OnMouseExit()
     {
-        Debug.Log("Trigger exit!");
-        Vector3 NewPosition = new Vector3(1225, 0, -9720);
-        Window.GetComponent<Transform>().position = NewPosition;
+        Window.SetActive(false);
     }
 }
