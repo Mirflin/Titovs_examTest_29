39a913d8c7b5e1815940379916181e7cebf874f2
Miirflin <mrmiirflinn@gmail.com>
2024-06-05 21:35:56 +0200

    Updated QuestionChange script(now all the thing for the questions are done)

--- Assets/Scripts/QuestionsChangeScript.cs
+++ Assets/Scripts/QuestionsChangeScript.cs
@@ -8,19 +8,39 @@ public class QuestionsChangeScript : MonoBehaviour
     public GameObject Buttons;
     public Text Number;
     public Text QuestionDes;
+    public Text Choose1;
+    public Text Choose2;
+    public Text Choose3;
+    public Text Choose4;
+    public Button Next;
 
-    
-    private int Question = 0;
-    private string[] Question_list = { };
-    private bool[] submited = new bool[15];
+    private int Question = 1;
+    private string DisplayedNumber = "1";
+    private string[] Question_list = { "quest1","quest2"};
+    private string[,] Question_anwers = new string[2, 4] { { "1", "2", "3", "4" }, { "da","net","danet","netda"} };
 
     private void Start()
     {
-        for(int i = 0; i < 15; i++)
-        {
-            submited[i] = false;
-        }
+        Number.text = Question.ToString();
+    }
+
+    public void ButtonClicked(int number)
+    {
+        Question = number-1;
+        DisplayedNumber = number.ToString();
+        GameObject child = Buttons.transform.GetChild(Question).gameObject;
+        QuestionDes.text = (string) Question_list[Question];
         Number.text = (Question+1).ToString();
+        Choose1.text = Question_anwers[Question,0];
+        Choose2.text = Question_anwers[Question,1];
+        Choose3.text = Question_anwers[Question,2];
+        Choose4.text = Question_anwers[Question,3];
+    }
+
+    public void NextButton()
+    {
+        Question += 1;
+        ButtonClicked(Question);
     }
 
 
